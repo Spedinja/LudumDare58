9a23960694c14c5e4f0b05a19b2e2b112650c7fe
Eos034 <44605630+Eos034@users.noreply.github.com>
2025-10-04 20:31:08 +0200

    TestScene

--- testScript.gd
+++ testScript.gd
@@ -3,6 +3,7 @@ extends Node2D
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Attack"):
 		var newBullet = projectile.instantiate()
-		newBullet.direction = Vector2(1,0)
 		newBullet.global_position = self.global_position + Vector2(300,300)
+		var direction = (get_global_mouse_position() - newBullet.global_position).normalized()
+		newBullet.direction = direction
 		self.add_child(newBullet)
