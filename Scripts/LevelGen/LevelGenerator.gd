df930bd6d5aa0c145e751f1f304b91c6c94021a2
UmbraLux <shadowcard@gmx.de>
2025-10-04 17:48:08 +0200

    first adjustements to algorithm

--- Scripts/LevelGen/LevelGenerator.gd
+++ Scripts/LevelGen/LevelGenerator.gd
@@ -11,6 +11,10 @@ class_name LevelGenerator
 @export var endRoom : RoomData
 @export var outerBoundRoom: RoomData
 
+#additions
+@export var max_rooms: int = 50
+@export var num_geckos: int = 0
+
 #vars
 var room: RoomData
 var gridRoom: GridRoom
@@ -31,15 +35,17 @@ func _ready():
 #algorithm to generate a level
 func genLevel():
 	init_grid()
-	generateMainPath()
-	#addBranches() # not needed a tthe moment, needs adjustments if we want to use it, as i want it to make deadends additions
+	#generateMainPath()
+	generateDungeon()
+	#addBranches()
+	addBranchesUntilMax()
 	fillGridWithMain()
 	assignRoomTemplatesBasedOnNeighbors()
 	placeStartRoom()
 	placeEndRoom()
-	#assignRoomTemplatesBasedOnNeighbors() #moved up so start and end room get set right
+	placeGeckoRooms()
 	instantiateRooms()
-	fixOuterBounds()
+	fixOuterBounds()	
 	return
 	
 #setup grids
@@ -56,52 +62,100 @@ func init_grid():
 	return
 
 #generate main path start-finish	
-func generateMainPath():
-	
-	#start room
-	visited[currPos.x][currPos.y] = true
-	mainPath.append(currPos)
-	
-	#next room
-	currPos = Vector2i(startRoomPos.x +1, startRoomPos.y)
-	visited[currPos.x][currPos.y] = true
-	mainPath.append(currPos)
-	
-	#generate mainPath
-	while currPos.x < WIDTH-1:
-		var options = []
-		if currPos.y > 0 and not visited[currPos.x][currPos.y - 1]:
-			options.append(Vector2i(0, -1)) #up
-		if currPos.y < HEIGHT - 1 and not visited[currPos.x][currPos.y + 1]:
-			options.append(Vector2i(0, 1)) #down
-		if currPos.x < WIDTH - 1 and not visited[currPos.x + 1][currPos.y]:
-			options.append(Vector2i(1, 0)) #right
-		
-		if options.is_empty():
-			break
-	
-		var move = options.pick_random()
-		currPos += move
-		visited[currPos.x][currPos.y] = true
-		
-		#var room = GridRoom.new()
-		#room.x = currPos.x
-		#room.y = currPos.y
-		#grid[currPos.x][currPos.y] = room
-		
-		mainPath.append(currPos)
-	
+#func generateMainPath():
+	#
+	##start room
+	#visited[currPos.x][currPos.y] = true
+	#mainPath.append(currPos)
+	#
+	##next room
+	#currPos = Vector2i(startRoomPos.x +1, startRoomPos.y)
+	#visited[currPos.x][currPos.y] = true
+	#mainPath.append(currPos)
+	#
+	##generate mainPath
+	#while currPos.x < WIDTH-1:
+		#var options = []
+		#if currPos.y > 0 and not visited[currPos.x][currPos.y - 1]:
+			#options.append(Vector2i(0, -1)) #up
+		#if currPos.y < HEIGHT - 1 and not visited[currPos.x][currPos.y + 1]:
+			#options.append(Vector2i(0, 1)) #down
+		#if currPos.x < WIDTH - 1 and not visited[currPos.x + 1][currPos.y]:
+			#options.append(Vector2i(1, 0)) #right
+		#
+		#if options.is_empty():
+			#break
+	#
+		#var move = options.pick_random()
+		#currPos += move
+		#visited[currPos.x][currPos.y] = true
+		#
+		##var room = GridRoom.new()
+		##room.x = currPos.x
+		##room.y = currPos.y
+		##grid[currPos.x][currPos.y] = room
+		#
+		#mainPath.append(currPos)
+
+#adjusted algorithm of old version	
+func generateDungeon():
+	# start room
+	visited[startRoomPos.x][startRoomPos.y] = true
+	mainPath.append(startRoomPos)
+
+	#branch in all 4 directions
+	for dir in [Vector2i(1,0), Vector2i(-1,0), Vector2i(0,1), Vector2i(0,-1)]:
+		if randf() < 0.75: #% chance to open this direction
+			weBeDrunkenWalkin(startRoomPos, dir)	
+
+#drunken walk algorithm
+func weBeDrunkenWalkin(start: Vector2i, dir: Vector2i):
+	var curr = start + dir
+	while isWithinBounds(curr) and not visited[curr.x][curr.y] and mainPath.size() < max_rooms:
+		visited[curr.x][curr.y] = true
+		mainPath.append(curr)
+
+		#rng turn
+		if randf() < 0.5:
+			dir = [Vector2i(1,0), Vector2i(-1,0), Vector2i(0,1), Vector2i(0,-1)].pick_random()
+
+		curr += dir
+
+
 #generates branches/additional rooms connecting to mainpath
 #should be changes if we want to use it, so only deadends get added
-func addBranches():
-	for room in mainPath.duplicate():
-		for dir in [Vector2i(0,-1), Vector2i(0,1),Vector2i(1,0),Vector2i(-1,0)]:
-			var n = room + dir
-			if isWithinBounds(n) and not visited[n.x][n.y] and randf() < 0.1:
-				visited[n.x][n.y] = true
-				mainPath.append(Vector2i(n.x,n.y))
+#func addBranches():
+	#for room in mainPath.duplicate():
+		#for dir in [Vector2i(0,-1), Vector2i(0,1),Vector2i(1,0),Vector2i(-1,0)]:
+			#var n = room + dir
+			#if isWithinBounds(n) and not visited[n.x][n.y] and randf() < 0.1:
+				#visited[n.x][n.y] = true
+				#mainPath.append(Vector2i(n.x,n.y))
+	#
+	#return mainPath
 	
-	return mainPath
+func addBranchesUntilMax():
+	var room_count = mainPath.size()
+	
+	while room_count < max_rooms:
+		var base_room = mainPath.pick_random()
+		var directions = [Vector2i(0,-1), Vector2i(0,1), Vector2i(1,0), Vector2i(-1,0)]
+		directions.shuffle()
+		
+		var placed = false
+		for dir in directions:
+			var n = base_room + dir
+			if isWithinBounds(n) and not visited[n.x][n.y] and randf() < 1.0:
+				visited[n.x][n.y] = true
+				mainPath.append(n)
+				room_count += 1
+				placed = true
+				break
+		
+		#cant place a new room at all (surrounded), break to avoid infinite loop
+		if not placed:
+			break
+		return
 	
 #setup base gridroom in the grid, only need to find and add a roomtemplate later
 func fillGridWithMain():
@@ -144,15 +198,47 @@ func placeStartRoom():
 
 #finds the furthest room away from start, and flags its + add roomdata
 func placeEndRoom():
-	var furthest = Vector2i(0, 0)
-	for x in range(WIDTH):
-		for y in range(HEIGHT):
-			if visited[x][y] and x > furthest.x:
-				furthest = Vector2i(x, y)
-
+	#var furthest = Vector2i(0, 0)
+	#for x in range(WIDTH):
+		#for y in range(HEIGHT):
+			#if visited[x][y] and x > furthest.x:
+				#furthest = Vector2i(x, y)
+#
+	var furthest = bfsFurthestRoom(startRoomPos) #added - above removed
 	var room = grid[furthest.x][furthest.y]
 	room.roomdata = null
 	room.roomdata = endRoom
+
+#use bfs to find furthest away point as end room
+func bfsFurthestRoom(start: Vector2i) -> Vector2i:
+	var queue = [start]
+	var dist = {}
+	dist[start] = 0
+	var furthest = start
+
+	while not queue.is_empty():
+		var current = queue.pop_front()
+		for dir in [Vector2i(1,0), Vector2i(-1,0), Vector2i(0,1), Vector2i(0,-1)]:
+			var n = current + dir
+			if isValidRoom(n.x, n.y) and not dist.has(n):
+				dist[n] = dist[current] + 1
+				queue.append(n)
+				if dist[n] > dist[furthest]:
+					furthest = n
+
+	return furthest
+
+func placeGeckoRooms():
+	var candidates: Array[Vector2i] = []
+	for pos in mainPath:
+		if pos != startRoomPos:
+			var grid_room = grid[pos.x][pos.y]
+			if grid_room and grid_room.roomdata != endRoom:
+				candidates.append(pos)
+
+	candidates.shuffle()
+	for i in range(min(num_geckos, candidates.size())):
+		grid[candidates[i].x][candidates[i].y].roomdata.isGeckoRoom = true
 
 #checks wether the position is inside the grid or not
 func isWithinBounds(pos: Vector2i) -> bool:
@@ -160,13 +246,34 @@ func isWithinBounds(pos: Vector2i) -> bool:
 	
 #finds and assigns possible neighbours of all rooms inside the grid
 func assignRoomTemplatesBasedOnNeighbors():
+	#for pos in mainPath:
+			#if !grid[pos.x][pos.y] == null:
+				#var connections = getRequiredConnections(pos.x,pos.y)
+				#var match = findTemplate(connections)
+				#if match:
+					#var room = grid[pos.x][pos.y]
+					#room.roomdata = match 
+	#return
+	
 	for pos in mainPath:
-			if !grid[pos.x][pos.y] == null:
-				var connections = getRequiredConnections(pos.x,pos.y)
-				var match = findTemplate(connections)
-				if match:
-					var room = grid[pos.x][pos.y]
-					room.roomdata = match 
+		var grid_room = grid[pos.x][pos.y]
+		if grid_room == null:
+			continue
+		
+		var connections = getRequiredConnections(pos.x, pos.y)
+		
+		# gecko check
+		if grid_room.roomdata.isGeckoRoom:
+			var shop_match = findTemplate(connections, true)
+			if shop_match:
+				grid_room.roomdata = shop_match
+			continue
+		
+		# normal room
+		var match = findTemplate(connections, false)
+		if match:
+			grid_room.roomdata = match
+		
 	return
 
 #gets the connetion the room at gridPos x,y needs to be setup
@@ -187,14 +294,28 @@ func isValidRoom(posX:int,posY:int) -> bool:
 	return posX >= 0 and posX < WIDTH and posY >= 0 and posY < HEIGHT and visited[posX][posY] == true
 
 #find a valid room template for placement
-func findTemplate(connections: Dictionary):
+func findTemplate(connections: Dictionary, requires_Gecko):
+	#var options = []
+	#for room in roomPool.rooms:
+		#if room.hasLeftNbr == connections["left"] and \
+			#room.hasRightNb == connections["right"] and \
+			#room.hasTopNb == connections["top"] and \
+			#room.hasBottomNb == connections["bottom"]:
+			#options.append(room)
+	#return options.pick_random()
 	var options = []
 	for room in roomPool.rooms:
+		if room.isGeckoRoom != requires_Gecko:
+			continue
+		
 		if room.hasLeftNbr == connections["left"] and \
 			room.hasRightNb == connections["right"] and \
 			room.hasTopNb == connections["top"] and \
 			room.hasBottomNb == connections["bottom"]:
 			options.append(room)
+	
+	if options.is_empty():
+		return null
 	return options.pick_random()
 
 #spawn/instantiate all rooms via roomdata in the grid
