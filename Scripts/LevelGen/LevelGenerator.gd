ec29c9280bbffaca2ecd0188872d319ccd452d05
UmbraLux <shadowcard@gmx.de>
2025-10-05 16:27:51 +0200

    changed enemy spawn in proc gen -> spawns not only in start room and checks max and minx numbers better instead of only max

--- Scripts/LevelGen/LevelGenerator.gd
+++ Scripts/LevelGen/LevelGenerator.gd
@@ -309,11 +309,18 @@ func instantiateRooms():
 			if room_scene is Room_Fragment:
 				room_scene.has_cagedGecko = grid_room.roomdata.isGeckoRoom
 				
+				var enemySetting:int
 				#enemy setting
-				if currEnemyCount <= maxEnemyCount && room_scene.spawn_count > 0:
-					var enemySetting = randi_range(minEnemyCount, min(maxEnemyCount, room_scene.spawn_count))
-					room_scene.spawn_count = enemySetting
-					currEnemyCount = currEnemyCount + enemySetting
+				if currEnemyCount < maxEnemyCount && room_scene.spawn_count > 0:
+					if currEnemyCount < minEnemyCount:
+						enemySetting = randi_range(0, room_scene.spawn_count)
+						room_scene.spawn_count = enemySetting
+						currEnemyCount = currEnemyCount + enemySetting
+					#otherwise random chance to add more (up to cap)
+					elif randf() < 0.5:
+						enemySetting = randi_range(0, room_scene.spawn_count)
+						room_scene.spawn_count = enemySetting
+						currEnemyCount += enemySetting
 			self.add_child(room_scene)
 	return
 
