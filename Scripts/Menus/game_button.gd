e1d4e23924ab97d06230417dda8dc1415c6e8196
Hannes Börner <hannes15@t-online.de>
2025-10-05 17:01:11 +0200

    added music

--- Scripts/Menus/game_button.gd
+++ Scripts/Menus/game_button.gd
@@ -3,6 +3,10 @@ class_name GameButton
 
 func _ready():
 	connect("mouse_entered", _on_hovered)
+	connect("pressed", _on_pressed)
 
 func _on_hovered():
 	SoundManager.play_ui_sound(SoundManager.ui_sounds.HOVER)
+	
+func _on_pressed():
+	SoundManager.play_ui_sound(SoundManager.ui_sounds.CLICK)
